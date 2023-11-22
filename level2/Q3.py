9866db2812ac23b3d95afb1e03e03a15e7871ca5
YOGESHWARAN D <124228144+YOGESHWARAN3645@users.noreply.github.com>
2023-11-22 18:09:16 +0530

    Create Q3.py

---
+++ level2/Q3.py
@@ -0,0 +1,14 @@
+#If his basic salary is less than Rs. 1500, then HRA = 10% of basic salary and DA = 90% of basic salary. 
+#If his salary is either equal to or above Rs. 1500, then HRA = Rs. 500 and DA = 98% of basic salary. 
+#If the employee's salary is input through the keyboard write a program to find his gross salary
+salary=float(input("Salary:"))
+if(salary<1500):
+  hra=(10/100)*salary
+  da=salary-hra
+  grossSalary=salary+hra+da
+  print("Gross salary : ",grossSalary)
+elif(salary>=1500):
+  hra=500
+  da=(98/100)*salary
+  grossSalary=salary+hra+da
+  print("Gross salary : ",grossSalary)
