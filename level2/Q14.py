6fe0bbd19b46da35e99fa0d2ea2ba4505659d05f
YOGESHWARAN D <124228144+YOGESHWARAN3645@users.noreply.github.com>
2023-11-23 18:25:59 +0530

    Create Q14.py

---
+++ level2/Q14.py
@@ -0,0 +1,6 @@
+#Any year is entered through the keyboard, 
+#write a program to determine whether the year is leap or not. 
+
+year=int(input("year:"))
+if(year%4==0):
+  print("it is an leap year")
