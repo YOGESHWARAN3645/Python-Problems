6f1d6e68880b3c226764e34d392c178c83872257
YOGESHWARAN D <124228144+YOGESHWARAN3645@users.noreply.github.com>
2023-11-24 08:45:05 +0530

    Delete Basic problems/Basic problems

--- Basic problems/Basic problems/Level2/Q8.py
+++
@@ -1,8 +0,0 @@
-#Any integer is input through the keyboard. 
-#Write a program to find out whether it is an odd number or even number. 
-#(Hint: Use the % (modulus) operator)
-num= int(input("num : "))
-if(num%2==0):
-  print("even")
-else:
-  print("odd")
