c8a876a362cc115e9db329b476275b3160aa8c3d
YOGESHWARAN D <124228144+YOGESHWARAN3645@users.noreply.github.com>
2023-11-24 08:47:04 +0530

    Delete level2 directory

--- level2/Q12.py
+++
@@ -1,6 +0,0 @@
- #Find the absolute value of a number entered through the keyboard.
-num=int(input("Num:"))
-if(num<0):
-  num= -1*num
- 
-print("Num:",num)
--- level2/Q14.py
+++
@@ -1,6 +0,0 @@
-#Any year is entered through the keyboard, 
-#write a program to determine whether the year is leap or not. 
-
-year=int(input("year:"))
-if(year%4==0):
-  print("it is an leap year")
