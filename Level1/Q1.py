46fe37e367919dbf3c0188e0409e659b7ff89516
YOGESHWARAN D <124228144+YOGESHWARAN3645@users.noreply.github.com>
2023-11-24 08:46:54 +0530

    Delete Level1 directory

--- Level1/Q1.py
+++
@@ -1,8 +0,0 @@
-#1. Ramesh's basic salary is input through the keyboard. 
-#His dearness allowance is 40% of basic salary, and house rent allowance is 20% of basic salary. 
-#Write a program to calculate his gross salary.
-
-basicSalary=int(input("Basic Salary:"))
-da=(40/100)*basicSalary
-ra=(20/100)*basicSalary
-grossSalary=basicSalary+da+ra
--- Level1/Q4.py
+++
@@ -1,6 +0,0 @@
-#Temperature of a city in Fahrenheit degrees is input through the keyboard. 
-#Write a program to convert this temperature into Centigrade degrees.
-
-fahrenheit=float(input("Fahrenheit:"))
-centigrade= (F - 32) * 5/9
-print("Centigrade",centigrade)
